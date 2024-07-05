49577ca12895c5b158f15db5e3645f9eba576461
brenthoang12 <brenthoang@gmail.com>
2024-07-05 17:58:19 +0700

    testing output

--- archieve/model_1/train.py
+++ archieve/model_1/train.py
@@ -4,6 +4,7 @@ import h5py
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
 from tensorflow.keras.optimizers import RMSprop
@@ -24,17 +25,19 @@ datagen = ImageDataGenerator(
 ###### augmente data for model training ######
 
 train_gen = datagen.flow_from_directory(
-    "model/altered/train/",
+    "archieve/original/train",
     target_size=(300, 300),
-    batch_size=32,
+    batch_size= 128,
+    classes = ["hotdog", "not_hotdog"],
     class_mode="binary"
 )
 
 
 validation_gen = datagen.flow_from_directory(
-    "model/altered/test/",
+    "archieve/original/test",
     target_size=(300, 300),
-    batch_size=32,
+    batch_size= 128,
+    classes = ["hotdog", "not_hotdog"],
     class_mode="binary"
 )
 
@@ -60,10 +63,10 @@ model.add(tf.keras.layers.MaxPooling2D(2,2))
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(512, activation='relu'))
 model.add(tf.keras.layers.Dropout(0.5))
-model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(2, activation='softmax'))
 
 model.compile(
-    loss='binary_crossentropy',
+    loss='sparse_categorical_crossentropy',
     optimizer=RMSprop(learning_rate=0.001),
     metrics=['accuracy']
 )
@@ -78,10 +81,21 @@ history = model.fit(
     #validation_steps=8,
     verbose=1)
 
-model.save('model/model.h5') # this is a dog shit model (have to come up with new ways)
+model.save('archieve/model_1/model.h5') # this is a dog shit model (have to come up with new ways)
 
-model.summary()
+img = image.load_img('archieve/model_1/02.jpeg', target_size=(300, 300)) 
 
+img_array = image.img_to_array(img)
+
+print(img_array.shape)
+
+img_array = img_array / 255.0
+
+print(img_array.shape)
+
+predictions = model.predict(img_array)
+
+print(predictions)
 
 # using colab to convert h5 to json
 
